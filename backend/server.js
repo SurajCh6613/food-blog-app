c1dca1e5e2f9c4cad60375ffcab654f5704cd2a3
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-11 10:09:10 +0530

    Initial setup

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+require("dotenv").config({ path: "./config/.env" });
+const express = require("express");
+const recipe = require("./routes/recipe");
+const connectDB = require("./config/connectionDB");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+connectDB();
+
+app.use("/recipe", recipe);
+
+app.listen(PORT, () => {
+  console.log(`Server running at PORT : ${PORT}`);
+});
