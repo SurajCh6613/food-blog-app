17df91db187f66aa40e6cf3a599a26d2002980eb
Suraj Chaudhary <surajchaudhary6613@gmail.com>
2025-06-13 13:28:25 +0530

    Navbar, Footer, Home Page banner added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import MainNavigation from "./components/MainNavigation";
+import axios, { all } from 'axios'
+const getAllRecipes = async ()=>{
+  let allRecipes = []
+  await axios.get('http://localhost:3000/recipe').then(res=>{
+    allRecipes = res.data
+  })
+  return allRecipes
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainNavigation />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+        loader:getAllRecipes
+      },
+    ],
+  },
+]);
 
 const App = () => {
   return (
-    <div className='text-3xl'>App</div>
-  )
-}
+    <>
+      <RouterProvider router={router}></RouterProvider>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import foodRecipe from "../assets/foodRecipe.png";
+import RecipeItems from "../components/RecipeItems";
+const Home = () => {
+  return (
+    <>
+      <section className="home">
+        <div className="left">
+          <h1>Food Recipe</h1>
+          <h5>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus
+            accusamus quos et voluptas repellat maiores voluptatem optio facere
+            tempora voluptatibus!
+          </h5>
+          <button>Share your Recipe</button>
+        </div>
+        <div className="right">
+          <img src={foodRecipe} alt="foodRecipe" width="320px" height="300px" />
+        </div>
+      </section>
+      <div className="bg">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path
+            fill="#0099ff"
+            fillOpacity="1"
+            d="M0,64L60,96C120,128,240,192,360,208C480,224,600,192,720,160C840,128,960,96,1080,96C1200,96,1320,128,1380,144L1440,160L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"
+          ></path>
+        </svg>
+      </div>
+
+      <div className="recipe">
+        <RecipeItems />
+      </div>
+    </>
+  );
+};
+
+export default Home;
